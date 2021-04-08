b0746a79df0dbf111eb8f382d5f1f73c83a92f9f
Balint Pekker <balint.pekker@cheppers.com>
2021-04-08 17:31:26 +0200

    Create a select list

--- README.md
+++ README.md
@@ -1,40 +1,27 @@
-# phpstorm-config
+# PhpStorm IDE Configuration
 
 This repository's goal is to share PhpStorm config between team members to keep everyone updated with the latest changes. This involves:
-* Code styles
-* Inspection profiles
-* Live templates
-* Plugins
-* Default database connections for [ddev](https://github.com/drud/ddev)
-* .editorconfig
 
-### Prerequisites
+Directory | Contents
+----------|---------
+codestyle | Code Style settings (Editor > Code Style)
+fileTemplates | File and Code Templates (Editor > File and Code Templates)
+inspection | Inspection profiles (Editor > Inspections)
+templates | Live templates (Editor > Live Templates)
 
-In order to be able to use phpcs globally, you have to have it installed globally with `composer`, also you need to configure the `Drupal` and `DrupalPractice` coding standard if you haven't done it before.
-
-```shell
-composer global require "squizlabs/php_codesniffer=*"
-composer global require drupal/coder
-export PATH="$PATH:$HOME/.composer/vendor/bin"
-phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
-```
-
-You can verify if your setup is working properly with running `phpcs -i` and checking if the coding standards mentioned above are installed.
-
-### Configure a settings repository
-On each computer where you want your settings to be applied, select **File | Manage IDE Settings | Settings Repository** from the main menu. Specify the URL of the repository you've created, and click **Overwrite Local**.
-
-You can click **Merge** if you want the repository to keep a combination of the remote settings and your local settings. If any conflicts are detected, a dialog will be displayed where you can resolve these conflicts.
+## Installation
+### 1. Install with configuring a settings repository (prefered)
+On each computer where you want your settings to be applied, select **File > Manage IDE Settings > Settings Repository** from the main menu. Specify the URL of the repository you've created, and click **Overwrite Local**.
 
 Your local settings will be automatically synchronized with the settings stored in the repository each time you perform an **Update Project** or a **Push** operation, or when you close your project or exit PhpStorm.
 
 Further information on how to configure these settings can be found [here](https://www.jetbrains.com/help/phpstorm/sharing-your-ide-settings.html#settings-repository).
 
-### Manual Setup
+### 2. Install Manually
+After cloning the repository, you can run the bash script which will take care of copying the files to the latest PhpStorm's configuration directory.
 
-If you are for some reason unable to configure the settings repository to overwrite your local configuration, you can do it manually by copying the files from the repository to the respective places.
+> NOTE: If you install the configuration manually instead of using this repository as the settings configuration in PhpStorm, the updates to this repository will not affect your local configuration.
 
-Run
+In order to copy the configuration files, please run
 `./update-config.bash`
 
-> You can add an alias to this file, or paste it into your `PATH` in order to execute it without having to use the extension. It is recommended to set up an alias for it, and run the script occasionally to check for updates.
--- update-config.bash
+++ update-config.bash
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# ANSI Color codes for better formatting.
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+# Check the OS the script is running on.
 platform='unknown'
 case "${OSTYPE}" in
   solaris*) platform='solaris' ;;
@@ -9,29 +16,53 @@ case "${OSTYPE}" in
   msys*)    platform='msys' ;;
 esac
 
+#
 # MacOS
+# ---
+# The script will search through the '/Applications' folder for directories using the 'PhpStorm' string to see if there
+# are more then 1 version installed then asks the user to select the one they want the configuration to be applied for.
+#
 if [ "${platform}" = "darwin" ] && [ -f "/Applications/PhpStorm.app/Contents/Info.plist" ]; then
-  PhpStormVersion=$(/usr/libexec/PlistBuddy -c "Print :JVMOptions:Properties:idea.paths.selector" /Applications/PhpStorm.app/Contents/Info.plist)
-  ConfigLibrary=~/Library/Application\ Support/JetBrains/"${PhpStormVersion}"
+  echo "Searching for installed PhpStorm versions. Please wait..."
+  # Use the built-in Input Field Separator to create the PHPSTORM_VERSIONS array.
+  IFS=$'\n' read -r -d '' -a PHPSTORM_VERSIONS <<< "$(find /Applications -type d -name "*PhpStorm*" -print 2>/dev/null)"
+  PS3="Select the PhpStorm version you want the configurations to be applied for: "
+  select APP_VERSION in "${PHPSTORM_VERSIONS[@]}"
+  do
+    [[ -n $APP_VERSION ]] || { echo "Invalid choice. Please try again." >&2; continue; }
+    CHOSEN_VERSION="$(echo "${APP_VERSION}" | rev | cut -d'/' -f1 | rev)"
+    # Use PlistBuddy to check the configuration folder for the chosen PhpStorm version.
+    PHPSTORM_VERSION="$(/usr/libexec/PlistBuddy -c 'Print :JVMOptions:Properties:idea.paths.selector' /Applications/"${CHOSEN_VERSION}"/Contents/Info.plist)"
+    break
+  done
+  CONFIG_LIBRARY=~/Library/Application\ Support/JetBrains/"${PHPSTORM_VERSION}"
 else
+   echo -e "${RED}Error!${NC}"
    echo "Couldn't fetch information from Info.plist"
 fi
 
 # Linux
-# @todo `~/.config/JetBrains/PhpStorm2020.3`
+# @todo
+if [ "${platform}" = "linux" ]; then
+  IFS=' ' read -r -a PHPSTORM_SCRIPTS <<< "$(whereis phpstorm)"
+  SCRIPT_LOCATION=$(for i in "${PHPSTORM_SCRIPTS[@]}";do echo "$i";done | grep phpstorm.sh)
+  # idea.properties -> idea.config.path -> if set, use it, if not, use the default `~/.config/JetBrains/PhpStorm2021.1`
+  echo "${SCRIPT_LOCATION}"
+fi
 
 # Check for updates first before copying anything.
 # @todo
 
 # Copy new files to the respective places.
-# @todo Ask the user if they are sure to run the script.
-if [ "${PhpStormVersion}" != "" ]; then
-  echo "Copying files to ${ConfigLibrary}"
-  # Find all files located in config folder that are not hidden filed or README or this script.
-  folders="$(find . -type d -not -path '*/\.*' -not -path './README.md' -not -path './update-config.bash' -not -path '.')"
-  for folder in ${folders}
+if [ "${PHPSTORM_VERSION}" != "" ]; then
+  echo ""
+  echo -e "Copying files to ${CYAN}'${CONFIG_LIBRARY}'${NC}"
+  # Find all files located in config folder that are not hidden files or README.md or this script.
+  FOLDERS="$(find . -type d -not -path '*/\.*' -not -path './README.md' -not -path './update-config.bash' -not -path '.')"
+  for FOLDER in ${FOLDERS}
   do
-    cp -Rf "${folder}" "${ConfigLibrary}"
+    cp -Rf "${FOLDER}" "${CONFIG_LIBRARY}"
   done
+  echo -e "${GREEN}Success!${NC}"
   echo "Your PHPStorm configuration has been updated successfully."
 fi
